4b7ac043a0194528b8a4deb5b6b0b20b5542c04c
SiyamakAbasnezhad <siyamak1981@gmail.com>
2020-04-12 22:37:02 +0430

    ShaliShomal

---
+++ Dashboard/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    allowImportExportEverywhere: true
+  },
+
+
+
+};
